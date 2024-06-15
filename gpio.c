767c82b117c4fb360fb5ca4b5617181aacb344aa
BABABOIBOI <evtushenko076@gmail.com>
2024-06-16 02:09:51 +0300

    Execute laboratory work

--- gpio.c
+++ gpio.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <gpiod.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdint.h>
 #include <stdbool.h>
-#include <string.h>
 #include <time.h>
+#include <stdint.h>
 
 #define LED_PIN_1 17  // GPIO pin for the first LED
 #define LED_PIN_2 27  // GPIO pin for the second LED
-#define LED_PIN_3 23  // GPIO pin for the second LED
+#define LED_PIN_3 23  // GPIO pin for the third LED
 #define BUTTON_PIN 22 // GPIO pin for the button
 
 #define BLINK_DELAY 3
 
 #define CONSUMER "Consumer"
 
-// Function to unexport GPIO pins
+struct Array {
+    int size;
+    struct gpiod_line **elements;
+};
 
 
 // Function to set up a GPIO line
@@ -32,11 +33,13 @@ struct gpiod_line *setup_gpio_line(struct gpiod_chip *chip, unsigned int line_nu
     if (direction == GPIOD_LINE_REQUEST_DIRECTION_INPUT) {
         if (gpiod_line_request_input(line, CONSUMER) < 0) {
             perror("Error setting line as input");
+            gpiod_line_release(line);
             return NULL;
         }
     } else {
         if (gpiod_line_request_output(line, CONSUMER, value) < 0) {
             perror("Error setting line as output");
+            gpiod_line_release(line);
             return NULL;
         }
     }
@@ -44,28 +47,37 @@ struct gpiod_line *setup_gpio_line(struct gpiod_chip *chip, unsigned int line_nu
     return line;
 }
 
-void blinkLed(struct gpiod_line *line, struct timespec *lastToggleTime) {
+void changeDiodesState(struct Array lines) {
+    for (int i = 0; i < lines.size; i++) {
+        struct gpiod_line *line = lines.elements[i];
+        gpiod_line_set_value(line, !gpiod_line_get_value(line));
+    }
+}
+
+// Function to blink an LED connected to a GPIO line
+void blinkLed(struct Array lines, struct timespec *lastToggleTime) {
     struct timespec currentTime;
     clock_gettime(CLOCK_MONOTONIC, &currentTime);
 
-    int periodNs = nanosInSec / BLINK_DELAY;
-    long long elapsedNs = (currentTime.tv_sec - lastToggleTime->tv_sec) * nanosInSec +
+    int periodNs = 1000000000 / BLINK_DELAY;
+    long long elapsedNs = (currentTime.tv_sec - lastToggleTime->tv_sec) * 1000000000 +
                           (currentTime.tv_nsec - lastToggleTime->tv_nsec);
 
     if (elapsedNs >= periodNs) {
-
-        gpiod_line_set_value(line, !gpiod_line_get_value(line));
+        changeDiodesState(lines);
         clock_gettime(CLOCK_MONOTONIC, lastToggleTime);
     }
 }
 
 _Bool previous_button_state = false;
-uint8_t button_presed = 0;
+uint8_t button_pressed = 0;
 
 int main() {
     struct gpiod_chip *chip;
     struct gpiod_line *led_line_1, *led_line_2, *led_line_3, *button_line;
-
+    struct timespec lastToggleTime;
+    clock_gettime(CLOCK_MONOTONIC, &lastToggleTime);
+	_Bool turnOff = false;
     // Open the GPIO chip (chip 0 is usually correct for Raspberry Pi)
     chip = gpiod_chip_open_by_number(0);
     if (!chip) {
@@ -86,45 +98,66 @@ int main() {
     button_line = setup_gpio_line(chip, BUTTON_PIN, GPIOD_LINE_REQUEST_DIRECTION_INPUT, 0);
     if (!button_line) return 1;
 
-    while (1) {
 
+    struct gpiod_line *oneDiode[] = {led_line_1};
+    struct gpiod_line *twoDiodes[] = {led_line_1, led_line_2};
+    struct gpiod_line *threeDiodes[] = {led_line_1, led_line_2, led_line_3};
+
+    struct Array one = {1, oneDiode};
+    struct Array two = {2, twoDiodes};
+    struct Array three = {3, threeDiodes};
+
+    while (1) {
         _Bool buttonState = gpiod_line_get_value(button_line);
 
-        if (previous_button_state == 1 && buttonState == 0) {
-            (button_presed < 5) ? (button_presed++) : (button_presed = 0);
+        if (previous_button_state == true && buttonState == false) {
+            if (button_pressed < 4)
+                button_pressed++;
+            else
+                button_pressed = 0;
         }
 
-        switch (button_presed) {
-            case 0: {
+        switch (button_pressed) {
+            case 0:
                 gpiod_line_set_value(led_line_1, 0);
                 gpiod_line_set_value(led_line_2, 0);
                 gpiod_line_set_value(led_line_3, 0);
+                //printf("zero case switch");
+				turnOff = false;
                 break;
-            }
-            case 1: {
-                blinkLed(led_line_1, &lastToggleTime);
+            case 1:
+                blinkLed(one, &lastToggleTime);
+                //printf("first case switch");
+				turnOff = false;
                 break;
-            }
-            case 2: {
-                blinkLed(led_line_1, &lastToggleTime);
-                blinkLed(led_line_2, &lastToggleTime);
+				
+            case 2:
+				if(!turnOff){
+					gpiod_line_set_value(led_line_1, 0);
+					turnOff = true;
+				}
+                blinkLed(two, &lastToggleTime);
+                //printf("second case switch");
                 break;
-            }
-            case 3: {
-                blinkLed(led_line_1, &lastToggleTime);
-                blinkLed(led_line_2, &lastToggleTime);
-                blinkLed(led_line_3, &lastToggleTime);
+            case 3:
+				if(turnOff){
+					gpiod_line_set_value(led_line_1, 0);
+					gpiod_line_set_value(led_line_2, 0);
+					turnOff = false;
+				}
+                blinkLed(three, &lastToggleTime);
+                //printf("third case switch");
                 break;
-            }
-            default: {
+            default:
                 gpiod_line_set_value(led_line_1, 1);
                 gpiod_line_set_value(led_line_2, 1);
                 gpiod_line_set_value(led_line_3, 1);
+                //printf("default case switch");
                 break;
-            }
         }
+
         previous_button_state = buttonState;
-        struct timespec delay = {0, 10000000L};
+        struct timespec delay = {0, 10000000L}; // 10 milliseconds
         nanosleep(&delay, NULL);
     }
 
@@ -139,4 +172,3 @@ int main() {
 
     return 0;
 }
-
--- gpio_control.c
+++ gpio_control.c
@@ -17,7 +17,10 @@
 #define INPUT "in"
 #define LOW "0"
 #define HIGH "1"
-
+struct Array {
+    int size;
+    char **elements;
+};
 // Функція для налаштування режиму піна
 void pinMode(const char pin[], const char mode[]) {
     FILE *sysfs_export;
@@ -89,78 +92,108 @@ void cleanUp(const char pin[]) {
     fwrite(pin, 1, strlen(pin), sysfs_unexport);
     fclose(sysfs_unexport);
 }
-void blinkLed(const char pin[], struct timespec *lastToggleTime) {
+void changeDiodesState(struct Array pins) {
+    for (int i = 0; i < pins.size; i++) {
+        char *pin = pins.elements[i];
+        char currentValue[2];
+            snprintf(currentValue, sizeof(currentValue), "%d", digitalRead(pin) == 0 ? 1 : 0);
+            digitalWrite(pin, currentValue);
+    }
+}
+void blinkLed(struct Array lines, struct timespec *lastToggleTime) {
     struct timespec currentTime;
     clock_gettime(CLOCK_MONOTONIC, &currentTime);
 
-        int periodNs = nanosInSec / BLINK_DELAY;
-        long long elapsedNs = (currentTime.tv_sec - lastToggleTime->tv_sec) * nanosInSec +
-                              (currentTime.tv_nsec - lastToggleTime->tv_nsec);
+    int periodNs = 1000000000 / BLINK_DELAY;
+    long long elapsedNs = (currentTime.tv_sec - lastToggleTime->tv_sec) * 1000000000 +
+                          (currentTime.tv_nsec - lastToggleTime->tv_nsec);
 
-        if (elapsedNs >= periodNs) {
-            char currentValue[2];
-            snprintf(currentValue, sizeof(currentValue), "%d", !digitalRead(pin));
-            digitalWrite(pin, currentValue);
-            clock_gettime(CLOCK_MONOTONIC, lastToggleTime);
-        }
+    if (elapsedNs >= periodNs) {
+        changeDiodesState(lines);
+        clock_gettime(CLOCK_MONOTONIC, lastToggleTime);
+    }
 }
+
 _Bool previous_button_state = false;
-uint8_t button_presed = 0;
+uint8_t button_pressed = 0;
 
 int main() {
-
-    // Очищаємо піни перед завершенням програми
+    // Очищаем пины перед завершением программы
     cleanUp(LED1_GPIO);
-    cleanUp(LED1_GPIO);
+    cleanUp(LED2_GPIO);
     cleanUp(LED3_GPIO);
     cleanUp(BUTTON_GPIO);
 
-    // Налаштовуємо піни
+    // Настраиваем пины
     pinMode(LED1_GPIO, OUTPUT);
-    pinMode(LED1_GPIO, OUTPUT);
+    pinMode(LED2_GPIO, OUTPUT);
     pinMode(LED3_GPIO, OUTPUT);
     pinMode(BUTTON_GPIO, INPUT);
+
     struct timespec lastToggleTime;
     clock_gettime(CLOCK_MONOTONIC, &lastToggleTime);
+	char *oneDiode[] = {LED1_GPIO};
+    char *twoDiodes[] = {LED1_GPIO, LED2_GPIO};
+    char *threeDiodes[] = {LED1_GPIO, LED2_GPIO, LED3_GPIO};
 
+    struct Array one = {1, oneDiode};
+    struct Array two = {2, twoDiodes};
+    struct Array three = {3, threeDiodes};
+	_Bool turnOff = false;
     while (1) {
-        // Зчитуємо стан кнопки
+        // Считываем состояние кнопки
         _Bool buttonState = digitalRead(BUTTON_GPIO);
 
-        if(previous_button_state == 1 && buttonState == 0){
-            (button_presed < 5)?(button_presed++):(button_presed=0);
+        // Обрабатываем нажатие кнопки
+        if (previous_button_state == 1 && buttonState == 0) {
+            button_pressed = (button_pressed < 4) ? (button_pressed + 1) : 0;
         }
 
-        switch (button_presed) {
-            case 0:{
+        // Выполняем действия в зависимости от состояния кнопки
+        switch (button_pressed) {
+            case 0: {
                 digitalWrite(LED1_GPIO, LOW);
                 digitalWrite(LED2_GPIO, LOW);
                 digitalWrite(LED3_GPIO, LOW);
                 break;
             }
-            case 1:{
-                blinkLed(LED1_GPIO,&lastToggleTime);
+            case 1: {
+                blinkLed(one, &lastToggleTime);
+				turnOff = false;
                 break;
             }
-            case 2:{
-                blinkLed(LED1_GPIO,&lastToggleTime);
-                blinkLed(LED2_GPIO,&lastToggleTime);
+            case 2: {
+				if(!turnOff){
+					digitalWrite(LED1_GPIO, LOW);
+					turnOff = true;
+				}
+                blinkLed(two, &lastToggleTime);
+                
                 break;
             }
-            case 3:{
-                blinkLed(LED1_GPIO,&lastToggleTime);
-                blinkLed(LED2_GPIO,&lastToggleTime);
-                blinkLed(LED3_GPIO,&lastToggleTime);
+            case 3: {
+				if(turnOff){
+					digitalWrite(LED1_GPIO, LOW);
+					digitalWrite(LED2_GPIO, LOW);
+					turnOff = false;
+				}
+                blinkLed(three, &lastToggleTime);
+
                 break;
             }
-            default:{
+            default: {
                 digitalWrite(LED1_GPIO, HIGH);
                 digitalWrite(LED2_GPIO, HIGH);
                 digitalWrite(LED3_GPIO, HIGH);
                 break;
             }
         }
+
+        // Обновляем состояние предыдущей кнопки
         previous_button_state = buttonState;
+
+        // Задержка для стабилизации цикла
+        usleep(10000); // 10 миллисекунд
     }
 
     return 0;
